afcc459360687d5121add0294705e7c4349166c6
Rahul8998 <99120393+Rahul8998@users.noreply.github.com>
2022-09-27 00:53:27 +0530

    Add files via upload

---
+++ dist/assets/routs.e28b266f.js
@@ -0,0 +1,1 @@
+import{b as l}from"./index.a153618e.js";import{u}from"./auth.e045ea5b.js";import"./axios.a905d66a.js";var p=l(({router:r,store:t})=>{r.beforeEach((e,n,o)=>{const a=u(t),i=!["/login"].includes(e.path),s=a.logedIn;i&&!s?o("/login"):e.name=="login"&&s?o("/"):o()})});export{p as default};
