b71e2f1720daab5e9d8a28e436369bf80286dc38
Rahul8998 <99120393+Rahul8998@users.noreply.github.com>
2022-11-05 22:31:41 +0530

    Delete dist/assets directory

--- dist/assets/routs.cfc25a78.js
+++
@@ -1,1 +0,0 @@
-import{b as l}from"./index.cb010043.js";import{u}from"./auth.47ef9b68.js";import"./axios.7c461726.js";var p=l(({router:r,store:t})=>{r.beforeEach((e,n,o)=>{const a=u(t),i=!["/login"].includes(e.path),s=a.logedIn;i&&!s?o("/login"):e.name=="login"&&s?o("/"):o()})});export{p as default};
--- dist/assets/use-quasar.031d06b8.js
+++
@@ -1,1 +0,0 @@
-import{l as a,ah as r}from"./index.cb010043.js";function u(){return a(r)}export{u};
