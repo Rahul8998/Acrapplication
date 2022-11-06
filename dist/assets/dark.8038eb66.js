0e949ed22d99a8d1fea520617639151603fdb347
Rahul8998 <99120393+Rahul8998@users.noreply.github.com>
2022-11-06 23:50:10 +0530

    Delete dist/assets directory

--- dist/assets/dark.8038eb66.js
+++
@@ -1,1 +0,0 @@
-import{P as t}from"./index.5ab2b0d1.js";const r=localStorage.getItem("dark")=="true";t.set(r);
--- dist/assets/routs.e6b327eb.js
+++
@@ -1,1 +0,0 @@
-import{b as l}from"./index.5ab2b0d1.js";import{u}from"./auth.945b05cc.js";import"./axios.90d7f1fb.js";var p=l(({router:r,store:t})=>{r.beforeEach((e,n,o)=>{const a=u(t),i=!["/login"].includes(e.path),s=a.logedIn;i&&!s?o("/login"):e.name=="login"&&s?o("/"):o()})});export{p as default};
