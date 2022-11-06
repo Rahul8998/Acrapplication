8fe7304dc05b170ab86514f58222057d062d9428
Rahul8998 <99120393+Rahul8998@users.noreply.github.com>
2022-11-06 22:23:19 +0530

    Delete dist/assets directory

--- dist/assets/dark.04fca814.js
+++
@@ -1,1 +0,0 @@
-import{P as t}from"./index.e8331cb9.js";const r=localStorage.getItem("dark")=="true";t.set(r);
--- dist/assets/use-quasar.79cd8cf2.js
+++
@@ -1,1 +0,0 @@
-import{l as a,ah as r}from"./index.e8331cb9.js";function u(){return a(r)}export{u};
