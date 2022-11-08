c43c0f4805e1afe1df8dfddd57ad2d6d5158fffe
Rahul8998 <99120393+Rahul8998@users.noreply.github.com>
2022-11-08 09:07:14 +0530

    Delete dist/assets directory

--- dist/assets/routs.3a158c7e.js
+++
@@ -1,1 +0,0 @@
-import{b as l}from"./index.7dd9e907.js";import{u}from"./auth.5b3e5bb9.js";import"./axios.4d09d6ea.js";var p=l(({router:r,store:t})=>{r.beforeEach((e,n,o)=>{const a=u(t),i=!["/login"].includes(e.path),s=a.logedIn;i&&!s?o("/login"):e.name=="login"&&s?o("/"):o()})});export{p as default};
